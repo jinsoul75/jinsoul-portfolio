7e16147128042af755b795102a88624530cc6ab9
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-09-01 17:53:59 +0900

    Chore: prettier설정

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    singleQuote: true,
+    // "" => ''
+    semi: true,
+    //코드 마지막에 세미클론
+    tabWidth: 2,
+    // 들여쓰기 너비는 2칸
+    trailingComma: 'all',
+    // 객체나 배열 키:값 뒤에 항상 콤마를 붙히도록
+    printWidth: 100,
+    // 코드 한줄이 default 80 => maximum 100칸
+    arrowParens: 'always',
+    // 화살표 함수가 하나의 매개변수를 받을 때 괄호를 필수로 formatting
+  };
